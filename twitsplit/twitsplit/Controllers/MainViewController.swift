cea146bead109b89cace256b6ead2fff49f039cb
Leon Yuu <khangvha@gmail.com>
2018-01-12 14:04:50 +0700

    Setup Code Base

--- twitsplit/twitsplit/Controllers/MainViewController.swift
+++ twitsplit/twitsplit/Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  twitsplit
 //
 //  Created by Leon Yuu on 1/12/18.
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ twitsplit/twitsplit/Globals.swift
@@ -0,0 +1,35 @@
+//
+//  Globals.swift
+//  twitsplit
+//
+//  Created by Leon Yuu on 1/12/18.
+//  Copyright © 2018 Leon Yuu. All rights reserved.
+//
+
+import UIKit
+
+// MARK : CONSTANTS
+struct ScreenSize {
+    static let SCREEN_WIDTH         = UIScreen.main.bounds.size.width
+    static let SCREEN_HEIGHT        = UIScreen.main.bounds.size.height
+    static let SCREEN_MAX_LENGTH    = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+    static let SCREEN_MIN_LENGTH    = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+}
+
+// MARK: - Devices
+struct DeviceType {
+    static let IS_IPHONE_4_OR_LESS  = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH < 568.0
+    static let IS_IPHONE_5          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 568.0
+    static let IS_IPHONE_6          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 667.0
+    static let IS_IPHONE_6P         = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0
+    static let IS_IPAD              = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1024.0
+}
+
+// MARK: - Others
+
+// MARK: - Supports
+func printDebug(message: String, filename: String = #file, function: String = #function, line: Int = #line) {
+    #if DEBUG
+        print("[\((filename as NSString).lastPathComponent):\(line)] \(function) - \(message)")
+    #endif
+}
