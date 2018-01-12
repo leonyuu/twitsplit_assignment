88a91ab61f935eb80cdea86d5ec891a3ee7bef2f
Leon Yuu <khangvha@gmail.com>
2018-01-12 12:26:16 +0700

    setup project base source code

---
+++ twitsplit/Pods/Target Support Files/Pods-twitsplit/Pods-twitsplit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_twitsplit : NSObject
+@end
+@implementation PodsDummy_Pods_twitsplit
+@end
---
+++ twitsplit/twitsplitTests/twitsplitTests.swift
@@ -0,0 +1,36 @@
+//
+//  twitsplitTests.swift
+//  twitsplitTests
+//
+//  Created by Leon Yuu on 1/12/18.
+//  Copyright © 2018 Leon Yuu. All rights reserved.
+//
+
+import XCTest
+@testable import twitsplit
+
+class twitsplitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
