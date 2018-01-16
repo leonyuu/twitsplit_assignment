59b991dae864615e6fdeea38eec576b1436b72a8
Leon Yuu <khangvha@gmail.com>
2018-01-16 21:14:06 +0700

    Update Readme file

--- twitsplit/twitsplit/Manager/DataBaseManager.swift
+++ twitsplit/twitsplit/Manager/DataBaseManager.swift
@@ -75,22 +75,6 @@ class DataBaseManager: NSObject {
     }
     
     // MARK: - Twit Post Data
-    public static func getTwitPostDataById(id: Int) -> TwitPostData? {
-        let app = UIApplication.shared.delegate as! AppDelegate
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TwitPostData")
-        request.returnsObjectsAsFaults = false
-        request.predicate = NSPredicate(format: "id == %d", id)
-        do {
-            let dataArray = try app.managedObjectContext.fetch(request) as! [TwitPostData]
-            if dataArray.count > 0 {
-                return dataArray[0]
-            }
-        } catch {
-            printDebug(message: "Error fetching Twit Post Data")
-        }
-        return nil
-    }
-    
     public static func getAllTwistPostData() -> [TwitPostData]?{
         let app = UIApplication.shared.delegate as! AppDelegate
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TwitPostData")
